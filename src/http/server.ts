ed0ef2e7ee0f216d2f610544b20c5da3af4f5cc8
Arthur <arthursousarabelo2@gmail.com>
2025-08-18 16:04:15 -0300

    fix: remover

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,18 +1,6 @@
-import { writeFile } from 'node:fs/promises'
-import { resolve } from 'node:path'
 import { env } from '../env/env.ts'
 import { app } from './app.ts'
 
 app.listen({ port: env.PORT, host: '0.0.0.0' }).then(() => {
   console.log('Running server!')
-})
-
-app.ready().then(() => {
-  const spec = app.swagger()
-
-  writeFile(
-    resolve(process.cwd(), 'swagger.json'),
-    JSON.stringify(spec, null, 2),
-    'utf-8'
-  )
 })
